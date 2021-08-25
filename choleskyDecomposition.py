9051a73e5f77efbe458630da9db71ffbbeed5189
mrcadu <carlosvicc14@gmail.com>
2021-08-25 14:06:54 -0300

    Tirando conversão para int do choleskyDecomposition

--- choleskyDecomposition.py
+++ choleskyDecomposition.py
@@ -1,8 +1,6 @@
-# Python3 program to decompose
-# a matrix using Cholesky
-# Decomposition
 import math
 from matrix import *
+
 
 def decompose(matrix, n):
     lower = [[0 for _ in range(n)]
@@ -17,13 +15,13 @@ def decompose(matrix, n):
             if j == i:
                 for k in range(j):
                     sum1 += pow(lower[j][k], 2)
-                lower[j][j] = int(math.sqrt(matrix[j][j] - sum1))
+                lower[j][j] = float(math.sqrt(abs(matrix[j][j] - sum1)))
             else:
                 # Evaluating L(i, j)
                 # using L(j, j)
                 for k in range(j):
                     sum1 += (lower[i][k] * lower[j][k])
                 if lower[j][j] > 0:
-                    lower[i][j] = int((matrix[i][j] - sum1) /
-                                      lower[j][j])
+                    lower[i][j] = float((matrix[i][j] - sum1) /
+                                        lower[j][j])
     return lower, transpose(lower)
